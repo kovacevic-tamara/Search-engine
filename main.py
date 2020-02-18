ec56a5b6554666ac20836cb61dd6526ed400975c
anja-meseldzic <aanja.meseldzic@gmail.com>
2020-02-18 12:33:34 +0100

    #or operacija primena

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from set import operation_not
+from set import operation_or
 from stablo import izbor
 
 if __name__ == '__main__':
@@ -8,6 +8,6 @@ if __name__ == '__main__':
 
    lista1={'dd.html':2,'bb.html':3}
    lista2={'aa.html':3,'bb.html':1,'cc.html':6}
-   print(operation_not(lista1,lista2))
+   print(operation_or(lista1,lista2))
 
 
--- set.py
+++ set.py
@@ -2,7 +2,7 @@
 def operation_and(listaRec1, listaRec2):
     resultSet = {}
     if listaRec1== False or listaRec2 == False:
-        return resultSet
+        print("Nema zajednickog rezultata pretrage.")
     for html1 in listaRec1:
         for html2 in listaRec2:
             if html1 == html2:
@@ -11,14 +11,20 @@ def operation_and(listaRec1, listaRec2):
     return  resultSet
 
 def operation_or(listaRec1,listaRec2):
-    resultSet=listaRec1
-
-    for html in listaRec2:
-        if html not in resultSet:
-            resultSet[html]=listaRec2[html]
-        else:
-            resultSet[html]=listaRec1[html]+listaRec2[html]
-    return resultSet
+    if listaRec1 == False and listaRec2 == False:
+        print("Nema rezultata pretrage")
+    elif listaRec1 == False:
+        resultSet = listaRec2
+    elif listaRec2 == False:
+        resultSet=listaRec1
+    else:
+        resultSet = listaRec1
+        for html in listaRec2:
+            if html not in resultSet:
+                resultSet[html]=listaRec2[html]
+            else:
+                resultSet[html]=listaRec1[html]+listaRec2[html]
+    return print(resultSet)
 
 #python not java, znaci daj sve gde je pajton a nije java
 def operation_not(listaRec1,listaRec2):
