6a3cd1e57d8451c74283c8fa31c56ee531018df3
Tamara Kovacevic <t.kovacevic98@gmail.com>
2020-02-20 23:14:26 +0100

    #heap_sort

--- sort.py
+++ sort.py
@@ -1,40 +1,37 @@
 def heap_sort(resultSet): #stize mi html stranica kao kljuc i rank kao vrednost
     list=[]
-    sorted_result={}
+    result={}
     for val in resultSet.values():
         list.append(val)
-    list=list.copy()
+
     build_heap(list)
     sorted_list=[]
+
     for _ in range(len(list)):
         list[0],list[-1]=list[-1],list[0]
         sorted_list.append(list.pop())
-        min_heapify(list,0)
+        heap_max(list,0)
 
-    sorted_list2=[]
-    for i in reversed(sorted_list):
-        sorted_list2.append(i)
-
-    for res in sorted_list2:
+    for res in sorted_list:
         for key,values in resultSet.items():
             if res==values:
-                sorted_result[key]=values
-    return  sorted_result
+                result[key]=values
 
+    return  result
 
 def build_heap(list):
     for i in reversed(range(len(list)//2)):
-        min_heapify(list,i)
+        heap_max(list,i)
 
-def min_heapify(list,i):
+def heap_max(list,i):
     left=2*i+1
     right=2*i+2
     length=len(list)-1
-    mini=i
-    if left<=length and list[i]>list[left]:
-        mini=left
-    if right<=length and list[mini]>list[right]:
-        mini=right
-    if mini!=i:
-        list[i],list[mini]=list[mini], list[i]
-        min_heapify(list,mini)
+    max_el=i
+    if left<=length and list[i]<list[left]:
+        max_el=left
+    if right<=length and list[max_el]<list[right]:
+        max_el=right
+    if max_el!=i:
+        list[i],list[max_el]=list[max_el], list[i]
+        heap_max(list,max_el)
